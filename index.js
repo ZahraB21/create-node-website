c57cc17b61247bc856900eb62798dd48da1fa1a6
Zahra <zbehfarshad@gmail.com>
2020-03-02 19:27:22 -0800

    Adding Images to the home page

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
-// const pug = require("pug");
 
 const port = 3000;
-
 const app = express();
 
+const superheros = [
+  { name: "HULK", image: "hulk.jpg" },
+  { name: "THOR", image: "thor.jpg" }
+];
+
 app.set("view engine", "pug");
-app.use(express.static("/public"));
-app.get("/", (req, res) => res.render("index", {}));
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("index", { superheros: superheros }));
 
 app.listen(port, err => {
   err
