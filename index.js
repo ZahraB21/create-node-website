e42f00541e38ece87898dc1bc0b4e1abfde1b6e7
Zahra <zbehfarshad@gmail.com>
2020-03-03 10:58:17 -0800

    Adding an individual page for each superhero

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const port = 3000;
 const app = express();
 
 const superheros = [
-  { name: "HULK", image: "hulk.jpg" },
-  { name: "THOR", image: "thor.jpg" }
+  { id: 1, name: "HULK", image: "hulk.jpg" },
+  { id: 2, name: "THOR", image: "thor.jpg" }
 ];
 
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("index", { superheros: superheros }));
+
+app.get("/superhero/:id", (req, res) => {
+  const id = +req.params.id;
+
+  let selectedSuperhero = superheros.filter(superhero => superhero.id === id);
+
+  res.render("superhero", { superhero: selectedSuperhero });
+});
 
 app.listen(port, err => {
   err
